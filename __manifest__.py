894f293557d22a2aa3923c6db53843e43fd17ffd
ypro <yurua21@gmail.com>
2022-05-20 10:16:58 -0400

    ADD manifes

--- __manifest__.py
+++ __manifest__.py
@@ -15,7 +15,7 @@
     # Check https://github.com/odoo/odoo/blob/13.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'account',
-    'version': '0.1',
+    'version': '0.31',
 
     # any module necessary for this one to work correctly
     'depends': ['base','account','product'],
